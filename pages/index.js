3dfa98ba33b3a3973923738f8dec6f5029429347
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-10-24 21:24:35 +0000

    Added ability to include a global css with ssr, plus added extra rules for xo w/badge for readme

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import stylesheet from 'styles/main.scss'
+
 export default () => (
   <div>
+    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     Hello there <span>MyBit</span>
     <style jsx>{`
       /* this style only applies to the span within lexical scope */
