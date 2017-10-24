7d8839e77fcd7f23ff134fc5e51bcb98707fd1c7
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-10-24 21:48:35 +0000

    Adding logo as SVG through babel-inline-svg plugin, adding linting w/prettier through react + xo plus new eslint rules

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
+import React from 'react'
 import stylesheet from 'styles/main.scss'
+import Logo from '../svgs/logo-white.svg'
 
 export default () => (
   <div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     Hello there <span>MyBit</span>
-    <style jsx>{`
-      /* this style only applies to the span within lexical scope */
-      span {
-        color: red;
-      }
-    `}</style>
+    <Logo />
+    <style jsx>
+      {`
+        /* this style only applies to the span within lexical scope */
+        span {
+          color: red;
+        }
+      `}
+    </style>
   </div>
 )
