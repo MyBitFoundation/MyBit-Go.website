a43d18256d433b4ad39e68a0708916780f994da1
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-10-28 19:09:31 +0000

    Added subscription form with proper signup list w/global input + button selector to ease styling form oriented objects despite how we integrate them

---
+++ components/constants/index.js
@@ -0,0 +1,24 @@
+export const formProps = {
+  action: '//mybit.us15.list-manage.com/subscribe/post?u=af48b1fdb5278fd9884338f23&id=dbcac41639',
+  messages: {
+    inputPlaceholder: "Email",
+    btnLabel: "Subscribe",
+    sending: "Signing up...",
+    success: "Thank you! We will update you about any MyBit developments",
+    error: "Oops, please try again with other email"
+  },
+  styles: {
+    sending: {
+      fontSize: 18,
+      color: "auto"
+    },
+    success: {
+      fontSize: 18,
+      color: "#3fd0ae"
+    },
+    error: {
+      fontSize: 18,
+      color: "red"
+    }
+  }
+}
--- components/hero.js
+++ components/hero.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import SubscribeFrom from 'react-mailchimp-subscribe'
 import stylesheet from './hero.scss'
+
+import { formProps } from './constants/index'
 
 export default () => (
   <div className='Hero row'>
@@ -8,9 +11,11 @@ export default () => (
       <h1 className='Hero__title'>Transforming Investing</h1>
       <h2 className='Hero__subtitle'><b>MyBit</b> enables anyone to directly invest in machines that generate revenue</h2>
       <p>
-        Using Ethereum smart contracts to eliminate the middleman, The MyBit platform unlocks
+        Using Ethereum smart contracts to eliminate the middleman, The <b>MyBit</b> platform unlocks
         billions of dollars in investment opportunities and pushes more profit back to people.
       </p>
+      <SubscribeFrom {...formProps} />
+      <span className='Hero__subscribe-tagline'>Subscribe for early access to Alpha and breaking news</span>
     </div>
   </div>
 )
