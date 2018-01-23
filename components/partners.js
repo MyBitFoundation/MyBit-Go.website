5e2a6d00967f5df4e6bd6aca713b12ea5ad823f2
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2018-01-23 17:48:33 +0000

    New partner: Alpine Mining

--- components/partners.js
+++ components/partners.js
@@ -3,6 +3,7 @@ import enterpriseEthereumAlliance from '../static/partners/eea.png'
 import emerginginsider from '../static/partners/ei.png'
 import meyerlustenbergerLachenalLtd from '../static/partners/mll.png'
 import arabco from '../static/partners/arabco.png'
+import alpine from '../static/partners/alpinemining.png'
 import stylesheet from './partners.scss'
 
 export default () => {
@@ -10,9 +11,10 @@ export default () => {
     { id: 'eea', img: enterpriseEthereumAlliance, area: 'Business', link: 'https://entethalliance.org/members/' }, 
     { id: 'ei', img: emerginginsider, area: 'Public Relations', link: 'http://emerginginsider.com/' },
     { id: 'mll', img: meyerlustenbergerLachenalLtd, area: 'Legal', link: 'http://www.mll-legal.com/' },
-    { id: 'arabco', img: arabco, area: 'Smart Tech', link: 'https://medium.com/mybit-dapp/mybit-partner-with-arabco-smart-technology-8a54d39f17de' }
+    { id: 'arabco', img: arabco, area: 'Smart Tech', link: 'https://medium.com/mybit-dapp/mybit-partner-with-arabco-smart-technology-8a54d39f17de' },
+    { id: 'alpine', img: alpine, area: 'Crypto Mining', link: 'https://alpinemining.ch/en/' }
   ].map(partner => (
-    <div key={partner.id} className='Partners__wrapper col-md-3 col-xs-12'>
+    <div key={partner.id} className='Partners__wrapper col-md-4 col-xs-12'>
       <a href={partner.link} target='_blank'>
         <img className={`Partners__img Partners__img--is-${partner.id}`} src={partner.img} />
         <span className='Partners__area'>{partner.area}</span>
