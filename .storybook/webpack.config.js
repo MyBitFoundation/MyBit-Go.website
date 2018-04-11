3d00869f38dab81dddaaa0147f511f97cc27f261
Mark Robson <mark.o.robson@gmail.com>
2018-04-11 16:12:02 +0100

    docs: adding storybook
    develop locally by running `npm run storybook`

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2 },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.resolve(__dirname, '..', 'node_modules')],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
