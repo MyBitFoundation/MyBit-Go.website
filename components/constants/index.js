2b0aa7285dcbc8ef784c4bb6823c4116f23bd7f0
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-04-26 07:58:09 -0500

    Refactored highlights to be one component only, exported content to be on consts for modularity, ensured stories would be able to display multiple layers of a component 🤓

--- components/constants/index.js
+++ components/constants/index.js
@@ -14,6 +14,25 @@ import oliverImg from '../../static/team/oliver.png'
 import blankImg from '../../static/team/blank.png'
 import joostImg from '../../static/team/joost.png'
 
+export const highlights = [
+  {
+    title: 'About Us',
+    content: `
+<p><b>MyBit is an IoT investment ecosystem powered by Ethereum</b>. Founded in Switzerland by industry veterans, MyBit believes that people should be able to follow their passion instead of having to work to survive.</p>
+<p>By redefining the way people generate income, MyBit strives to democratise financial services so everyone has equal access to investment opportunities. This becomes critical when the future machine economy automates 800 million jobs by 2030 (Mckinsey, 2017) and the average person, whose job is automated, has no ability to participate in this new economy.</p>
+<p>MyBit provides an ecosystem for the upcoming Trillion dollar IoT industry (Forbes, 2017), with the belief that everyone should have an equal opportunity to participate in this revolution.</p>`
+  }, {
+    title: 'Our core conviction',
+    content: `
+<p>Although we all come from different countries, cultures and (professional) backgrounds there is one thing that connects us. We all share a common vision; to improve the life of everyone.</p>
+<p>We have seen the vast potential of IoT and automation but are aware that this will have major repercussions for the role humans play in the future economy.
+Instead of fighting this development we have chosen to embrace it.
+</p>
+<p>By utilising our shared resources we are building the first decentralised ecosystem that enables humans to play an active role in the economy of tomorrow.</p>
+<p>MyBit offers everyone to either invest or manage revenue generating machines over the blockchain, resulting in a new way to generate income in an automated world. MyBit lets the machines work for you again so you can enjoy the things that are truly important..</p>`
+  }
+];
+
 export const blank = {
   name: 'Careers',
   job: 'at MyBit',
--- components/highlights.js
+++ components/highlights.js
@@ -2,34 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import stylesheet from './highlights.scss';
 
-class Highlights extends Component {
+export const Highlight = ({ title, content }) => (
+  <section className="Highlight">
+    <article key={title} className="Highlight__card">
+      <h2 className="Highlight__card-title">{title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </article>
+  </section>
+)
+
+export class Highlights extends Component {
   render() {
+    const { highlights } = this.props;
     return (
-      <section className="Highlights">
+      <div className='Highlights'>
         <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         {
-          this.props.highlights.map(highlight => (
-            <article key={highlight.title} className="Highlights__card">
-              <h2 className="Highlights__card--title">{highlight.title}</h2>
-              <div dangerouslySetInnerHTML={{ __html: highlight.content }} />
-            </article>
-          )
+          highlights.map(highlight => (
+            <Highlight
+              key={highlight.title}
+              title={highlight.title}
+              content={highlight.content}
+            />)
           )
         }
-      </section>
+      </div>
     );
   };
 }
 
+Highlight.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
+}
+
 Highlights.propTypes = {
-  highlights: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired
-    })
-  )
+  highlights: PropTypes.arrayOf(PropTypes.element)
 };
 
 Highlights.defaultProps = [];
-
-export default Highlights;
