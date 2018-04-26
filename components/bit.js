325dfe762992e4d21af57bd9d789f60ea4789a30
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-04-26 08:58:45 -0500

    Small bit 🔹

---
+++ components/bit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import stylesheet from './bit.scss';
+
+export const Bit = () => {
+  return (
+    <div className='Bit'>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </div>
+  )
+}
