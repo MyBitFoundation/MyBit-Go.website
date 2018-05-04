adac54d0b0df10098299a38ea6edbd4733a1f567
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-05-04 10:41:25 -0500

    Added actual content to the landing page to reflect progress using the new container component to wrap around left and right nodes

--- components/layout/container.js
+++ components/layout/container.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import stylesheet from './container.scss'
 
-export const Container = ({ children, isStyled = false }) => {
+export const Container = ({ children, leftNode, rightNode, isStyled = false }) => {
   const containerClass = classNames({
     'Container': true,
     'Container--is-styled': isStyled
@@ -12,8 +12,8 @@ export const Container = ({ children, isStyled = false }) => {
     <div className={containerClass}>
       <div className='Container__wrapper'>
         <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-        <div className='Container__helper-left'/>
-        <div className='Container__helper-right'/>
+        <div className='Container__helper-left'>{ leftNode }</div>
+        <div className='Container__helper-right'>{ rightNode }</div>
         { children }
       </div>
     </div>
@@ -23,9 +23,13 @@ export const Container = ({ children, isStyled = false }) => {
 Container.propTypes = {
   isStyled: PropTypes.bool,
   children: PropTypes.node.isRequired,
+  leftNode: PropTypes.node,
+  rightNode: PropTypes.node,
 }
 
 Container.defaultProps = {
   isStyled: false,
+  leftNode: <div />,
+  rightNode: <div />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import stylesheet from 'styles/main.scss'
 import { default as Layout } from '../components/layout/layout'
+import { Container } from '../components/layout/container'
 
-import { Highlights } from '../components/highlights'
+import { Highlights, Highlight } from '../components/highlights'
 import { diamondHighlights } from '../components/constants'
 
 export default class Index extends React.Component {
@@ -11,6 +12,38 @@ export default class Index extends React.Component {
       <Layout>
         <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         <Highlights highlights={diamondHighlights} isDiamond />
+        <div className="grid__container" style={{ marginTop: '-500px' }}>
+          <Container
+            isStyled
+            leftNode={
+              <Highlight
+                title={'Investors'}
+                content={`<p>Gain direct access to the 11.1 Trillion
+                  dollar machine economy.</p>`}
+                icon="MyBitInvestorsIcon"
+                isDiamond
+                style={{ margin: 'auto 0', paddingLeft: '50px' }}
+                isTransparent
+              />
+            }
+            rightNode={
+              <Highlight
+                title={'Asset Manager'}
+                content={`<p>Profit from operating and maintaining
+                  IoT devices.</p>`}
+                icon="MyBitAssetManagerIcon"
+                isDiamond
+                style={{
+                  margin: 'auto 0',
+                  paddingRight: '50px',
+                  right: 0,
+                  left: 'auto'
+                }}
+                isTransparent
+              />
+            }
+          />
+        </div>
       </Layout>
     )
   }
