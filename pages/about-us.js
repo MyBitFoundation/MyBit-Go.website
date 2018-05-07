81193081e0914715523c0dcde05f0b352f4128de
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-05-07 04:35:01 -0500

    Added secondary pages setup

---
+++ pages/about-us.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import stylesheet from 'styles/main.scss'
+import { default as Layout } from '../components/layout/layout'
+
+import { Highlights } from '../components/highlights'
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+        <div
+          className="grid__container"
+          style={{
+            position: 'relative',
+            height: '600px'
+          }}
+        >
+          <Highlights
+            highlights={[
+              {
+                title: 'About Us Page',
+                isDiamond: true,
+                isLight: true,
+                isTransparent: true,
+                isBig: true,
+                style: { margin: 'auto' }
+              }
+            ]}
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
