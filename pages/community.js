08a5558a4c0acc365d4677e6d61610ce03e7302e
connorblockchain <connorblockchain@protonmail.com>
2018-05-10 01:08:27 +0200

    start community

--- pages/community.js
+++ pages/community.js
@@ -3,31 +3,22 @@ import stylesheet from 'styles/main.scss'
 import { default as Layout } from '../components/layout/layout'
 
 import { Highlights } from '../components/highlights'
+import { Header } from '../components/header'
 
 export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-        <div
-          className="grid__container"
-          style={{
-            position: 'relative',
-            height: '600px'
-          }}
-        >
-          <Highlights
-            highlights={[
-              {
-                title: 'Community Page',
-                isDiamond: true,
-                isLight: true,
-                isTransparent: true,
-                isBig: true,
-                style: { margin: 'auto' }
-              }
-            ]}
-          />
+        <div style={{ maxWidth: '1650px', margin: '0 auto' }}>
+          <style dangerouslySetInnerHTML={{ __html: stylesheet}} />
+          <div
+            className="grid__container"
+            style={{
+              height: '273px'
+            }}
+          >
+            <Header isLight={false}/>
+          </div>
         </div>
       </Layout>
     )
