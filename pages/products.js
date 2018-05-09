251f4a3613c4ad6ca4e85f4cc9d7912c65e2308c
connorblockchain <connorblockchain@protonmail.com>
2018-05-09 23:04:28 +0200

    stylesheet needs fixed

--- pages/products.js
+++ pages/products.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { default as Layout } from '../components/layout/layout'
 
-import stylesheet from '../components/wallets.scss'
+import stylesheet from 'styles/main.scss'
+import walletStylesheet from '../components/wallets.scss'
 import buttonStyleSheet from '../components/button.scss'
 
 import { Highlights } from '../components/highlights'
@@ -18,82 +19,83 @@ export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <style dangerouslySetInnerHTML={{ __html: stylesheet, __html: buttonStyleSheet }} />
-        <div
-          className="grid__container"
-          style={{
-            height: '273px'
-          }}
-        >
-          <Header />
-        </div>
-        <div
-          style={{
-            marginTop: '50px'
-          }}
-        >
-          <Highlights highlights={mediaHighlights} />
-        </div>
-        <div
-          style={{
-            marginTop: '75px'
-          }}
-        >
-          <Media media={media} />
-        </div>
-        <div
-          className="grid__container"
-          style={{ width: '100%', margin: 'auto', marginTop: '119px' }}
-        >
-          <Highlights
-            highlights={[
-              {
-                title: 'Native Wallets',
-                isLight: true,
-                isCentered: true,
-                isTransparent: true,
-                isFullWidth: true
-              }
-            ]}
-          />
-        </div>
-        <div
-          style={{
-            marginLeft: '210px'
-          }}
-        >
-          <Wallets Wallets={wallets} />
-        </div>
-        <div
-          style={{
-            marginTop: '40px'
-          }}
-        >
-          <MediaCTA
-            title="Are you an app developer?"
-            content="<p>Learn about being rewarded for developing for MyBit.</p>"
-            button={[
-              <Button
-                key="buttonA"
-                label="Learn more"
-                isLight
-                onClick={action('button-click')}
-              />,
-              <Button
-                key="buttonB"
-                label="Learn even more"
-                isLight
-                onClick={action('button-click')}
-              />
-            ]}
-          />
-        </div>
-        <div
-          className="grid__container"
-          style={{ marginTop: '134px', margin: 'auto' }}
-        >
-          <MyBitFooter />
-        </div>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet, __html: walletStylesheet }} />
+          <div
+            className="grid__container"
+            style={{
+              height: '273px'
+            }}
+          >
+            <Header />
+          </div>
+          <div
+            style={{
+              marginTop: '50px'
+            }}
+          >
+            <Highlights highlights={mediaHighlights} />
+          </div>
+          <div
+            style={{
+              marginTop: '75px'
+            }}
+          >
+            <Media media={media} />
+          </div>
+          <div
+            className="grid__container"
+            style={{ width: '100%', margin: 'auto', marginTop: '119px' }}
+          >
+            <Highlights
+              highlights={[
+                {
+                  title: 'Native Wallets',
+                  isLight: true,
+                  isCentered: true,
+                  isTransparent: true,
+                  isFullWidth: true
+                }
+              ]}
+            />
+          </div>
+          <div
+            style={{
+              marginLeft: '210px'
+            }}
+          >
+            <Wallets Wallets={wallets} />
+          </div>
+          <div
+            style={{
+              marginTop: '40px'
+            }}
+          >
+          <style dangerouslySetInnerHTML={{ __html: buttonStyleSheet }} />
+            <MediaCTA
+              title="Are you an app developer?"
+              content="<p>Learn about being rewarded for developing for MyBit.</p>"
+              button={[
+                <Button
+                  key="buttonA"
+                  label="Learn more"
+                  isLight
+                  onClick={action('button-click')}
+                />,
+                <Button
+                  key="buttonB"
+                  label="Learn even more"
+                  isLight
+                  onClick={action('button-click')}
+                />
+              ]}
+            />
+          </div>
+          <div
+            className="grid__container"
+            style={{ marginTop: '134px', margin: 'auto' }}
+          >
+            <MyBitFooter />
+          </div>
       </Layout>
     )
   }
