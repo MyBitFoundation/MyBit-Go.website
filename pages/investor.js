41ec2afa8f673f20b26f538acfe7078c89ae9f0d
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-05-09 18:21:57 +0100

    Moved consts to const file

--- pages/investor.js
+++ pages/investor.js
@@ -8,120 +8,17 @@ import { MyBitFooter } from '../components/footer/footer'
 import Roi from './static/svgs/other/roi.svg'
 import Cube from './static/svgs/other/cube.svg'
 import IndustryValueMobile from './static/assets/Industry Value Graph Mobile v2.png'
+import {
+  mediaWhy,
+  mediaVerticals,
+  highlights,
+  stats,
+  mediaWho
+} from './constants/investor'
 
 export default class Index extends React.Component {
   render() {
-    const mediaWhy = [
-      {
-        title: 'Why invest in IoT?',
-        content:
-          '<p>The Internet changed humanity significantly. Now there is a new technological revolution on its way. It is predicted that by 2025, 50 billion IoT devices will generate an astonishing 11.1 trillion $ in revenue.  Every industry and occupation will be affected under the fast development of autonomous machines. Mckinsey (2017) calculated that 50% of all global working-hours can be automated with currently demonstrated technology already. This machine transition will have an impact on 30% of all jobs by 2030. MyBit provides an ecosystem for the upcoming 11.1 Trillion dollar IoT industry, with the belief that everyone should have an equal opportunity to participate in this revolution.</p>',
-        iconClassName: ''
-      }
-    ]
-
-    const mediaVerticals = [
-      {
-        title: 'Target IoT verticals',
-        content:
-          '<p>The MyBit Ecosystem functions as a multi-industrial investment platform. </br>This means it is applicable to all markets, across all industries, in all countries.</p>',
-        iconClassName: '',
-        isCentered: true
-      }
-    ]
-
-    const content = [
-      {
-        title: 'Transportation',
-        content:
-          '<b>Delivery drones</b><b>Self driving taxis</b><b>Transport trucks</b>'
-      },
-      {
-        title: 'Financial',
-        content:
-          '<b>Cryptocurrency ATMs</b><b>Cryptocurrency Mining  Masternodes</b>'
-      },
-      {
-        title: 'Manufacturing',
-        content: '<b>3D printing</b><b>Robotics</b>'
-      },
-      {
-        title: 'Healthcare',
-        content: '<b>Diagnostics</b><b>Surgical</b>'
-      },
-      {
-        title: 'Real Estate',
-        content: '<b>Rental properties</b><b>Storage</b><b>Co-working space</b>'
-      },
-      {
-        title: 'Energy',
-        content: '<b>Solar</b><b>Wind</b><b>Charging Stations</b>'
-      },
-      {
-        title: 'Retail',
-        content: '<b>Vending machines</b><b>Automated kioks/stores</b>'
-      },
-      {
-        title: 'Agriculture',
-        content: '<b>Farming equipment</b><b>Automated harvesting</b>'
-      },
-      {
-        title: 'The future',
-        content:
-          '<b>The MyBit platform will grow together with the expanding IoT industry. Adding new devices and revenue models to existing IoT devices.</b>'
-      }
-    ]
-
-    const stats = [
-      {
-        area: 'Advanced Energy',
-        value: '1.4',
-        height: '10px'
-      },
-      {
-        area: 'Information & communication',
-        value: '1.7',
-        height: '13px'
-      },
-      {
-        area: 'Health',
-        value: '2',
-        height: '19px'
-      },
-      {
-        area: 'Transport and storage',
-        value: '2.4',
-        height: '25px'
-      },
-      {
-        area: 'Construction',
-        value: '2.5',
-        height: '35px'
-      },
-      {
-        area: 'Agriculture',
-        value: '11',
-        height: '96px'
-      },
-
-      {
-        area: 'Wholesale and retail',
-        value: '20',
-        height: '171px'
-      },
-      {
-        area: 'Manufacturing',
-        value: '24',
-        height: '210px'
-      },
-      {
-        area: 'Real estate',
-        value: '217',
-        height: '249px'
-      }
-    ]
-
-    const highlightsToRender = content.map(details => (
+    const highlightsToRender = highlights.map(details => (
       <Highlight
         key={details.title}
         title={details.title}
@@ -169,15 +66,6 @@ export default class Index extends React.Component {
 
     const mediaIndustriesMobile = [
       { ...mediaContent, content: industryValueMobileToRender }
-    ]
-
-    const mediaWho = [
-      {
-        title: 'Who can Invest?',
-        content:
-          '<p>Anyone can use the MyBit Platform. Unlike traditional investment funds that require unrealistic amounts of capital for 99% of people, MyBit ensures everyone access to the best opportunities. We believe in a fair and open market, driven by technology that actually benefits people. By using the most advanced blockchain technologies, MyBit revolutionized investing as whole. MyBit is investing made efficient, secure and fast for everyone.</p>',
-        iconClassName: ''
-      }
     ]
 
     return (
