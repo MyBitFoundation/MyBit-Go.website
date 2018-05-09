f2d3d126e02ec19ea9c8ae91d843a45990987811
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-05-09 09:46:35 -0500

    Minor fixes for all components and refactored page to be a more modular and readable page

---
+++ components/constants/industries.js
@@ -0,0 +1,26 @@
+export const industries = [
+  {
+    title: 'Agriculture',
+    content: `<p>Our ecosystem succeeds with cutting
+    edge technology and an amazing community</p>`,
+    icon: 'agriculture',
+    isLight: false,
+    isThin: true
+  },
+  {
+    title: 'Manufacturing',
+    content: `<p>Our ecosystem succeeds with cutting
+    edge technology and an amazing community</p>`,
+    icon: 'manufacturing',
+    isLight: false,
+    isThin: true
+  },
+  {
+    title: 'Logistics',
+    content: `<p>Our ecosystem succeeds with cutting
+    edge technology and an amazing community</p>`,
+    icon: 'logistics',
+    isLight: false,
+    isThin: true
+  }
+]
