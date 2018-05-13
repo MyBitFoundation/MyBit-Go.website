97c9ead7d06f2a167ed3eb786eef51c00d5e1511
Mark Robson <mark.o.robson@gmail.com>
2018-05-13 13:14:44 +0100

    Feature/event cards (#54)
    * feature: event cards
    
    * modification: fixing responsiveness. Bug when > 4 events
    
    * fix: fixed component styles for about page
    
    * fix: struggling with angled background. Temp fix.

--- components/highlights.js
+++ components/highlights.js
@@ -87,6 +87,7 @@ export const Highlight = ({
   link = null,
   style,
   isDiamond = false,
+  isRectangle = false,
   isLight = false,
   isCentered = false,
   isTransparent = false,
@@ -98,6 +99,7 @@ export const Highlight = ({
   const highlightWrapperClass = classNames({
     'Highlight__wrapper': true,
     'Highlight__wrapper--is-diamond': isDiamond,
+    'Highlight__wrapper--is-rectangle': isRectangle,
     'Highlight__wrapper--is-light': isLight,
     'Highlight__wrapper--is-transparent': isTransparent,
     'Highlight__wrapper--is-thin': isThin,
@@ -111,6 +113,7 @@ export const Highlight = ({
   const highlightCardClass = classNames({
     'Highlight__card': true,
     'Highlight__card--is-diamond': isDiamond,
+    'Highlight__card--is-rectangle': isRectangle,
     'Highlight__card--is-light': isLight,
     'Highlight__card--is-big': isBig,
     'Highlight__card--is-medium': isMedium,
@@ -159,11 +162,13 @@ export class Highlights extends Component {
       highlights,
       hasAlternateColors = true,
       startsFromLight = true,
-      isDiamond
+      isDiamond,
+      isRectangle
     } = this.props
     const highlightGroupClass = classNames({
       'Highlight__group': true,
-      'Highlight__group--is-diamond-group': isDiamond
+      'Highlight__group--is-diamond-group': isDiamond,
+      'Highlight__group--is-rectangle-group': isRectangle
     })
     return (
       <section className={highlightGroupClass}>
@@ -177,6 +182,7 @@ export class Highlights extends Component {
                 (index + 1) % 2 === (startsFromLight ? 1 : 0))
             }
             {...highlight}
+            isRectangle
           />
         ))}
       </section>
@@ -194,6 +200,7 @@ Highlight.propTypes = {
   content: PropTypes.string.isRequired,
   icon: PropTypes.string,
   link: PropTypes.string,
+  isRectangle: PropTypes.bool,
   isDiamond: PropTypes.bool,
   isLight: PropTypes.bool,
   isCentered: PropTypes.bool,
@@ -206,6 +213,7 @@ Highlight.propTypes = {
 }
 
 Highlight.defaultProps = {
+  isRectangle: false,
   isDiamond: false,
   isLight: false,
   isCentered: false,
@@ -223,7 +231,8 @@ Highlights.propTypes = {
   highlights: PropTypes.arrayOf(PropTypes.element),
   hasAlternateColors: PropTypes.bool,
   startsFromLight: PropTypes.bool,
-  isDiamond: PropTypes.bool
+  isDiamond: PropTypes.bool,
+  isRectangle: PropTypes.bool,
 }
 
 Highlights.defaultProps = []
