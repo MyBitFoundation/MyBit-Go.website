1414b06e60b9d5ea5a0440bcd445b63c543852ea
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-06-05 07:16:36 +0100

    feature/press-kit-download (#67)
    * Initial commit
    
    * Renaming press kit to have a cleaner url for web

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -79,6 +79,9 @@ export const MyBitFooter = () => {
     }, {
       title: 'Explorer',
       url: 'https://ethplorer.io/address/0x5d60d8d7eF6d37E16EBABc324de3bE57f135e0BC'
+    }, {
+      title: 'Press Kit',
+      url: "./static/files/MyBit_Press_Kit.zip"
     }]}
   />
   }, {
