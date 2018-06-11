e0cdbaa7ab495d485125f89da81044bd3cdf4397
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-06-11 12:42:09 +0100

    new header menu in all pages

--- components/constants/menu.js
+++ components/constants/menu.js
@@ -1,13 +1,17 @@
 export const mobileMenu = [{
-    name: "Ecosystem",
-    path: "community"
+    name: "Products",
+    path: "products"
   },{
     name: "Token",
     path: "access-layer"
   },{
     name: "About",
     path: "about-us"
-}];
+  }
+  ,{
+    name: "Test Alpha",
+    path: "https://goo.gl/forms/sv5AdzacXZnwq5r02"
+  }];
 
 
 export const headerMenu = [{
