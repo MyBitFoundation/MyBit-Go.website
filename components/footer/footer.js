f8caa60992d38fc9e2a6bb3670eede5b12fead90
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-06-14 16:15:59 +0100

    updated press kit

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -74,7 +74,7 @@ export const MyBitFooter = () => {
       url: 'https://ethplorer.io/address/0x5d60d8d7eF6d37E16EBABc324de3bE57f135e0BC'
     }, {
       title: 'Press Kit',
-      url: "./static/files/MyBit_Press_Kit.zip"
+      url: "./static/files/MyBit Press Kit.zip"
     }, {
       title: 'Whitepaper',
       url: 'https://files.mybit.io/files/MyBit_Whitepaper_v3.0.0.pdf'
