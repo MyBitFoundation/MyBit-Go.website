f796f1593d639bf01060cdf8fce46bc0c2738fc2
StevenJNPearce <stevenjnpearce@gmail.com>
2019-04-16 03:10:35 +0800

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Root, Routes } from 'react-static'
 import '../static/css/app.css'
 import '../static/css/tachyons-mybit.css'
 //import Menu from '@containers/Menu'
-import Foooter from '@containers/Footer'
+import Foooter from '@mybit/ui.footer'
 
 function App() {
   return (
@@ -18,7 +18,8 @@ function App() {
           style={{ borderColor: '#D9D9D9' }}
           className="bb mh2 border-1"
         />
-        <Foooter />
+        <div className="pt3"/>
+        <Foooter isGo={true} />
       </div>
     </Root>
   )
