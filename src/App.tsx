0555157166841be3ca059a630abca37ffea27231
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-12-06 20:49:32 +0100

    Update app to use footer package

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Root, Routes } from 'react-static'
 import '../static/css/app.css'
 import '../static/css/tachyons-mybit.css'
 //import Menu from '@containers/Menu'
-import Footer from './containers/Footer'
+import Foooter from '@mybit/ui.footer'	
 import BannerDao from '@components/BannerDao';
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
           className="bb mh2 border-1"
         />
         <div style={{maxWidth: '1128px'}} className="pt3 center">
-          <Footer />
+          <Foooter isGo={true} />
         </div>
       </div>
     </Root>
