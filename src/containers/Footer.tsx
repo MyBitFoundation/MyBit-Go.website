1981ce8cbeb0ce6e25971f2cbf731b1bb419476d
Mulenga Bowa <mulengabowa53@gmail.com>
2020-01-23 22:37:16 +0200

    fix margin

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -13,120 +13,13 @@ export default class Menu extends React.Component {
     return (
       <>
         <div className="ph2 pt4 flex-ns flex-row-ns flex-wrap-m justify-between-ns">
-          <div className="pv3 pv0-ns order-1-l">
-            <STitle>MyBit</STitle>
-            <SButton target="_top" href="https://app.mybit.io/">
-              LAUNCH MYBIT
-            </SButton>
-            <div className="pt5" />
-            <STitle>Follow us</STitle>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="pv1 pr1"
-              href="https://t.me/mybitio"
-            >
-              <img src={telegram} alt="telegram" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="pv1 pr1"
-              href="https://www.reddit.com/user/MyBit_DApp/"
-            >
-              <img src={reddit} alt="reddit" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="pv1 pr1"
-              href="https://medium.com/mybit-dapp"
-            >
-              <img src={medium} alt="medium" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="pv1 pr1"
-              href="https://twitter.com/MyBit_DApp"
-            >
-              <img src={twitter} alt="twitter" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="pv1 pr1"
-              href="https://www.facebook.com/MyBitDApp/"
-            >
-              <img src={facebook} alt="facebook" />
-            </a>
-          </div>
-          <SAddress>
-            <img className="pb1" src={mybit} alt="mybit logo" />
-            <div className="dib-ns">
-              MyBit is operated and maintained by a DAO
-              <div>DAO address:</div>
-              <SAnchor
-                target="_blank"
-                rel="noreferrer"
-                href="https://mainnet.aragon.org/#/0xcD3d9b832BfF15E0a519610372c6AAC651872DdE/"
-              >
-                0xcD3d9b832BfF15E0a519610372c6AAC651872DdE
-              </SAnchor>
-              <div className="pb2" />
-              <SAnchor
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/MyBitFoundation/MyBit.io/blob/develop/TOC.md"
-              >
-                Terms & Conditions
-              </SAnchor>
-              <div className="pb2" />
-            </div>
-          </SAddress>
-          <div className="flex-ns flex-row-ns justify-between-ns w-100 w-50-l pt3-m pt0-l order-0-l">
+          <div className="flex-ns flex-row-ns justify-between-ns w-100 pt3-m pt0-l order-0-l">
             <div className="pb3 pb0-ns">
               <STitle>MyBit</STitle>
               <SLink to="/about">About</SLink>
               <SLink to="/howitworks">How it Works</SLink>
               <SLink to="/dao">DAO</SLink>
-              <SLink to="/participate">Participate</SLink>
-              <SAnchor
-                target="_blank"
-                rel="noreferrer"
-                href="https://learn.mybit.io/"
-              >
-                Knowledge Base
-              </SAnchor>
               <SAnchor href="https://t.me/mybitio">Contact</SAnchor>
-            </div>
-            <div className="pb3 pb0-ns">
-              <STitle>contribute</STitle>
-              <SAnchor
-                target="_top"
-                rel="noreferrer"
-                href="https://app.mybit.io/"
-              >
-                {' '}
-                MyBit
-              </SAnchor>
-              <SAnchor
-                target="_blank"
-                rel="noreferrer"
-                href="https://mydax.io/"
-              >
-                {' '}
-                MYDAX{' '}
-              </SAnchor>
-              <SAnchor
-                target="_blank"
-                rel="noreferrer"
-                href="https://task.market/"
-              >
-                {' '}
-                Task Market{' '}
-              </SAnchor>
-              <SLink to="/tools">Other dApps</SLink>
             </div>
             <div className="pb2 pb0-ns">
               <STitle>resources</STitle>
@@ -159,6 +52,77 @@ export default class Menu extends React.Component {
                 whitepaper
               </SAnchor>
             </div>
+            <div className="pv3 pv0-nsg">
+              <STitle>MyBit</STitle>
+              <SButton target="_top" href="https://app.mybit.io/">
+                LAUNCH MYBIT
+              </SButton>
+              <div className="pt5" />
+              <STitle>Follow us</STitle>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="pv1 pr1"
+                href="https://t.me/mybitio"
+              >
+                <img src={telegram} alt="telegram" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="pv1 pr1"
+                href="https://www.reddit.com/user/MyBit_DApp/"
+              >
+                <img src={reddit} alt="reddit" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="pv1 pr1"
+                href="https://medium.com/mybit-dapp"
+              >
+                <img src={medium} alt="medium" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="pv1 pr1"
+                href="https://twitter.com/MyBit_DApp"
+              >
+                <img src={twitter} alt="twitter" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="pv1 pr1"
+                href="https://www.facebook.com/MyBitDApp/"
+              >
+                <img src={facebook} alt="facebook" />
+              </a>
+            </div>
+            <SAddress>
+              <img className="pb1" src={mybit} alt="mybit logo" />
+              <div className="dib-ns">
+                MyBit is operated and maintained by a DAO
+                <div>DAO address:</div>
+                <SAnchor
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://mainnet.aragon.org/#/0xcD3d9b832BfF15E0a519610372c6AAC651872DdE/"
+                >
+                  0xcD3d9b832BfF15E0a519610372c6AAC651872DdE
+                </SAnchor>
+                <div className="pb2" />
+                <SAnchor
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/MyBitFoundation/MyBit.io/blob/develop/TOC.md"
+                >
+                  Terms & Conditions
+                </SAnchor>
+                <div className="pb2" />
+              </div>
+            </SAddress>
           </div>
         </div>
       </>
