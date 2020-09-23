b12305c3b961b1f0b80725e6062998383df6d8d4
bobmybit <69350042+bobmybit@users.noreply.github.com>
2020-09-23 06:43:56 -0400

    Updated footer with new DAO link

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -104,13 +104,13 @@ export default class Footer extends React.Component {
               <img className="pb1" src={mybit} alt="mybit logo" />
               <div className="dib-ns">
                 MyBit is operated and maintained by a DAO
-                <div>DAO address:</div>
+                <div>DAO Link:</div>
                 <SAnchor
                   target="_blank"
                   rel="noreferrer"
-                  href="https://mainnet.aragon.org/#/0xcD3d9b832BfF15E0a519610372c6AAC651872DdE/"
+                  href="https://snapshot.page/#/mybit/community"
                 >
-                  0xcD3d9b832BfF15E0a519610372c6AAC651872DdE
+                  https://snapshot.page/#/mybit/community
                 </SAnchor>
                 <div className="pb2" />
                 <SAnchor
